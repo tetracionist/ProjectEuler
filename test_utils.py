8db7ac9d424ca4d377779974d8d3cfd73c8f35f6
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-04-08 18:07:55 +0100

    Create test_utils.py

---
+++ test_utils.py
@@ -0,0 +1,10 @@
+# need to edit this every time I add a new solution
+import time
+
+# solution to https://projecteuler.net/problem=32
+from project-euler.solutions.Pandigital-Products-32 import pandigitalSum
+
+def test_problem_32:
+  start_time = time.time()
+  pandigitalSum("123456789")
+    
