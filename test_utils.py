4fb10df9f24f3a4cfbb5c33a34938cf67a266060
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-04-08 18:12:51 +0100

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -4,7 +4,9 @@ import time
 # solution to https://projecteuler.net/problem=32
 from ProjecEuler.solutions.PandigitalProducts32 import pandigitalSum
 
-def test_problem_32:
+def test_problem_32():
   start_time = time.time()
   pandigitalSum("123456789")
+  
+print("pandigitalSum finished in --- %s seconds ---" % (time.time() - start_time))
     
