584ce434b7646db6b1d02cb6cb594c089de834db
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-04-08 18:47:19 +0100

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -6,8 +6,10 @@ import os
 from solutions.PandigitalProducts import pandigitalSum
 start_time = time()
 
+solution_32 = os.environ['SOLUTION_32']
+
 def test_problem_32():
-  assert pandigitalSum("123456789") == os.environ['SOLUTION_32']
+  assert pandigitalSum("123456789") == solution_32
   
 print("pandigitalSum finished in --- %s seconds ---" % (time() - start_time))
     
