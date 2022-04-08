e8bd3536828a5e776e0118fc99bcb6f4ef010295
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2022-04-08 18:10:01 +0100

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -2,7 +2,7 @@
 import time
 
 # solution to https://projecteuler.net/problem=32
-from project-euler.solutions.Pandigital-Products-32 import pandigitalSum
+from ProjecEuler.solutions.Pandigital-Products-32 import pandigitalSum
 
 def test_problem_32:
   start_time = time.time()
