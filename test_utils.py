255149ffb4089bd71420b7ec84f06f91515d26c2
Alexander Wornast <tetracionist@gmail.com>
2022-04-08 18:45:37 +0100

    Added answers in the form of secrets

--- test_utils.py
+++ test_utils.py
@@ -1,12 +1,13 @@
 # need to edit this every time I add a new solution
 from time import time
+import os
 
 # solution to https://projecteuler.net/problem=32
 from solutions.PandigitalProducts import pandigitalSum
 start_time = time()
 
 def test_problem_32():
-  assert pandigitalSum("123456789") == ${{ secrets.solution_32 }}
+  assert pandigitalSum("123456789") == os.environ['SOLUTION_32']
   
 print("pandigitalSum finished in --- %s seconds ---" % (time() - start_time))
     
